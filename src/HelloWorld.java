e54173899c96ce661bc66e8ead6f39f443298657
twipsta <twipsta@gmail.com>
2014-07-09 08:05:54 +1000

    Test Commit
    My First Commit :panda_face:

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		String test = "Zermagerd Im a TEST!";
+		System.out.println(test);
+		tests("A Function Call");
+	}
+	
+	public static void tests(String teststring){
+		System.out.println(teststring);
+	}
+
+}
